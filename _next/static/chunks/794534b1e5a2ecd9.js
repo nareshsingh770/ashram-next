80b400c867478793fe085c4d2b79a478445f4fc2
Naresh Singh <nsingh@gmail.com>
2025-11-02 15:35:36 +0530

    Updates

---
+++ _next/static/chunks/794534b1e5a2ecd9.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/e425695b2f62eed2.js",
+  "static/chunks/turbopack-ea706601627ecd60.js"
+])
---
+++ _next/static/fzSEcpurGS4lWNnwfvukW/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/794534b1e5a2ecd9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ashram-next//_next/:path+",
+        "destination": "/ashram-next/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
