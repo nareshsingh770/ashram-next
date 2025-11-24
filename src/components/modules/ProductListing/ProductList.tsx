a8236a29f228d5e08148fc7c34dc63ead71bccc2
Naresh Singh <nsingh@gmail.com>
2025-11-24 19:34:13 +0530

    calling PDF file from backend

--- src/components/modules/ProductListing/ProductList.tsx
+++ src/components/modules/ProductListing/ProductList.tsx
@@ -4,12 +4,31 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store";
 import { getBooks } from "@/store/slices/bookSlice";
+import { booksAPI } from "@/services/api";
+import PDFReader from "../PdfReader";
 
 const ProductList = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const [books, setBooks] = React.useState<string | null>(null);
+  const fetchBook = async () => {
+    const response = await booksAPI.getBooks();
+    return response.data;
+  };
   useEffect(() => {
-    dispatch(getBooks());
+    const fetchBooksData = async () => {
+      try {
+        const books = await fetchBook();
+        const blob = new Blob([books], { type: "application/pdf" });
+        const blobUrl = URL.createObjectURL(blob);
+        setBooks(blobUrl);
+      } catch (error) {
+        console.error("Failed to fetch books:", error);
+      }
+    };
+
+    fetchBooksData();
   }, []);
+
   return (
     <div className="layout-spacing mx-auto px-6 py-32 text-center">
       <Heading
